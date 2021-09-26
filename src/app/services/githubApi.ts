4e3ea8e888a0f6e9a4515f68460c6d28980363e4
Julian Ćwirko <julian.cwirko@gmail.com>
2021-09-26 23:42:41 +0200

    remove encode uri

--- src/app/services/githubApi.ts
+++ src/app/services/githubApi.ts
@@ -7,17 +7,14 @@ export const githubApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.github.com/' }),
   endpoints: (builder) => ({
     getUsers: builder.query<{ items: GithubUser[] }, string>({
-      query: (query) =>
-        encodeURIComponent(`search/users?q=${query}&per_page=5`),
+      query: (query) => `search/users?q=${query}&per_page=5`,
     }),
     getReposByUsername: builder.query<
       GithubRepo[],
       { username: string; page: number }
     >({
       query: ({ username, page = 1 }) =>
-        encodeURIComponent(
-          `users/${username}/repos?per_page=5&sort=updated&page=${page}`
-        ),
+        `users/${username}/repos?per_page=5&sort=updated&page=${page}`,
     }),
   }),
 });
